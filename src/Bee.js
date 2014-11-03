80eea0ed6f1daa914ed22228e08a89c5a3e6ed82
SWES <swesleyandloki@gmail.com>
2014-11-03 12:14:44 -0800

    taxonomy to test

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,10 @@
 var Bee = function() {
+	Grub.call(this);
+	this.age = 5;
+	this.color = 'yellow';
+	this.job = 'keep on growing';
+
 };
 
+Bee.Prototype = Object.create(Grub.prototype);
+Bee.Prototype.constructor = Bee;
--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,16 @@
 var RetiredForagerBee = function() {
+	ForagerBee(this);
+	this.age = 40;
+	this.job = gamble;
+	this.canFly = false;
+	this.color = 'grey';
 };
+
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+RetiredForagerBee.prototype.forage = return ("I am too old, let me play cards instead");
+RetiredForagerBee.prototype.gamble = function(treasure){
+	this.treasureChest.push(treasure);
+};
+
 
